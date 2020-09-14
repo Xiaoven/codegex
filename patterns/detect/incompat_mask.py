9156cf993ee27d1e4240b176b53ee422faf0d810
EricLee543 <ericlee54@foxmail.com>
2020-09-15 00:54:12 +0800

    实现了BIT_SIGNED_CHECK和BIT_AND_ZZ
    具体的文档见markdown EricLee543.md

--- patterns/detect/incompat_mask.py
+++ patterns/detect/incompat_mask.py
@@ -5,9 +5,11 @@ from patterns.bug_instance import BugInstance
 import regex
 
 
-class ChekBitSigned(Detector):
+class BitSignedCheckAndBitAndZZ(Detector):
     def __init__(self):
-        self.regexp = regex.compile('\(\s*((?:(?P<aux1>\((?:[^()]++|(?&aux1))*\))|[\w.])++)\s*&\s*((?:(?&aux1)|[\w.])+)\s*\)\s*>\s*0')
+        self.regexpSign = regex.compile('\(\s*((?:(?P<aux1>\((?:[^()]++|(?&aux1))*\))|[\w.])++)\s*&\s*((?:(?&aux1)|[\w.])+)\s*\)\s*>\s*0')
+        self.regexpZZ = regex.compile(
+            '\(\s*((?:(?P<aux1>\((?:[^()]++|(?&aux1))*\))|[\w.])++)\s*&\s*0\s*\)\s*==\s*0')
 
     def _visit_patch(self, patch):
         file_name = patch.name
@@ -24,9 +26,14 @@ class ChekBitSigned(Detector):
 
                 if is_comment(line_content):
                     continue
-                contains = self.regexp.search(line_content)
-                if contains:
+                containsSign = self.regexpSign.search(line_content)
+                containsZZ = self.regexpZZ.search(line_content)
+                if containsSign:
                     bug_ins = BugInstance("BIT_SIGNED_CHECK", Priorities.IGNORE_PRIORITY, file_name,
                                           hunk.lines[i].lineno[1], 'There may be a sign mistake. You '
                                                                    'can replace >0 with !=0 ')
+                    self.bug_accumulator.append(bug_ins)
+                if containsZZ:
+                    bug_ins = BugInstance("BIT_AND_ZZ", Priorities.HIGH_PRIORITY, file_name,
+                                          hunk.lines[i].lineno[1], 'It will always be equal like \'var & 0 == 0\'')
                     self.bug_accumulator.append(bug_ins)
