f7ffd5808bd60bca25ce3829e709f61036b5932d
wuqihan837 <69723926+wuqihan837@users.noreply.github.com>
2020-09-15 23:04:12 +0800

    wqh

---
+++ patterns/detect/stcal_static_simple_date_format_instance.py
@@ -0,0 +1,36 @@
+import re
+
+
+
+from patterns.detectors import Detector
+
+from patterns.bug_instance import BugInstance
+
+import patterns.priorities as Priorities
+
+from patterns.utils import is_comment
+
+
+class StcalStaticSimpleDateFormatInstance(Detector):
+    def __init__(self):
+        self.regexp = re.compile('static\s*.*\s*SimpleDateFormat\s*.*\s*=\s*new\s*SimpleDateFormat\(')
+    
+    def _visit_patch(self, patch):
+        for hunk in patch:
+            for i in range(len(hunk.lines)):
+                if i in hunk.dellines:
+                    continue
+
+                line_content = hunk.lines[i].content
+                if i in hunk.addlines:
+                    line_content = line_content[1:]  # remove "+"
+
+                   
+                if is_comment(line_content):
+                    continue
+                
+                match = self.regexp.search(line_content)
+                if match:
+                    confidence = Priorities.LOW_PRIORITY
+                    self.bug_accumulator.append(BugInstance('STCAL_STATIC_SIMPLE_DATE_FORMAT_INSTANCE', confidence,patch.name, hunk.lines[i].lineno[1],'Static DateFormat'))
+
