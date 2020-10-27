33a32927bd86aee7e328444fe62329faa2db7e42
xiaoven <xiaoven.cheung@gmail.com>
2020-10-27 11:33:49 +0800

    Modify main.py to scan pull requests
    log pull request info at the last line of log files

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import json
 import glob
 
+from patterns.detect.cnt_rough_constant_value import FindRoughConstants
+from patterns.detect.find_finalize_invocations import FindFinalizeInvocations
+from patterns.detect.find_ref_comparison import FindRefComparison
+from patterns.detect.find_unrelated_types_in_generic_container import FindUnrelatedTypesInGenericContainer
+from patterns.detect.format_string_checker import FormatStringChecker
+from patterns.detect.imse_dont_catch_imse import DontCatchIllegalMonitorStateException
+from patterns.detect.infinite_recursive_loop import InfiniteRecursiveLoop
+from patterns.detect.inheritance_unsafe_get_resource import InheritanceUnsafeGetResource
+from patterns.detect.naming import Naming
+from patterns.detect.serializable_idiom import SerializableIdiom
+from patterns.detect.static_calendar_detector import StaticCalendarDetector
 from rparser import Patch
 from patterns.detect.dumb_methods import DumbMethods
-from patterns.utils import logger
+from patterns.utils import logger, logger_add, TRACE
+
+# 25 patterns
+DETECTORS = [FindRoughConstants(), DumbMethods(), FindFinalizeInvocations(),
+             FindRefComparison(), FindUnrelatedTypesInGenericContainer(), FormatStringChecker(),
+             DontCatchIllegalMonitorStateException(), InfiniteRecursiveLoop(), InheritanceUnsafeGetResource(),
+             Naming(),SerializableIdiom(),StaticCalendarDetector()]
 
 
 def get_modified_patchset(path):
@@ -26,11 +43,27 @@ def get_modified_patchset(path):
 if __name__ == '__main__':
     root = 'Java'
     paths = glob.glob(f'{root}/**/*.json', recursive=True)
+
+    # logger name
+    logger.remove(TRACE)
+    cnt = 1
+    trace = logger_add('out/', f'{cnt}.log')
+    has_bug = False
+
     for p in paths:
+        if has_bug:
+            logger.remove(trace)
+            cnt += 1
+            trace = logger_add('out/', f'{cnt}.log')
+            has_bug = False
+
         patchset = get_modified_patchset(p)
         if patchset:
-            detector = DumbMethods() # run different detector on Java pull requests
-            detector.visit(patchset)
-            if detector.bug_accumulator:
-                logger.info(p)
-            detector.report()
+            for detector in DETECTORS:
+                detector.visit(patchset)
+                if detector.bug_accumulator:
+                    detector.report()
+                    has_bug = True
+
+        if has_bug:
+            logger.info(p)
--- patterns/utils.py
+++ patterns/utils.py
@@ -3,7 +3,7 @@ import os
 
 LOG_PATH = 'log'
 os.makedirs(LOG_PATH, exist_ok=True)
-logger.add(LOG_PATH + '/{time}.log')
+TRACE = logger.add(LOG_PATH + '/{time}.log')
 
 
 def is_comment(content: str):
@@ -12,3 +12,12 @@ def is_comment(content: str):
             strip_content.startswith('*'):
         return True
     return False
+
+
+def create_missing_dirs(path: str):
+    os.makedirs(path, exist_ok=True)
+
+
+def logger_add(path: str, name:str):
+    create_missing_dirs(path)
+    return logger.add(path+name)
