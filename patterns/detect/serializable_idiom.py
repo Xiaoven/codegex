bfca2d2b844d242f0a1d9ebf738b8e2ecd54702f
Xiaoven <55338943+Xiaoven@users.noreply.github.com>
2020-11-05 16:54:07 +0800

    fix false positive of definition of serialVersionID (#40)
    - Only detect long or int type
    - refactor the code

--- patterns/detect/serializable_idiom.py
+++ patterns/detect/serializable_idiom.py
@@ -1,51 +1,52 @@
 import re
 
-from patterns.detectors import Detector
+from patterns.detectors import ParentDetector, SubDetector
 from patterns.bug_instance import BugInstance
 import patterns.priorities as Priorities
 from patterns.utils import is_comment
 
 
-class SerializableIdiom(Detector):
+class SerializableIdiom(ParentDetector):
     def __init__(self):
-        self.pattern = re.compile('\s+serialVersionUID(?!\w)')
-
-    def _visit_patch(self, patch):
-        for hunk in patch:
-            for i in range(len(hunk.lines)):
-                # detect all lines in the patch rather than the addition
-                if i in hunk.dellines:
-                    continue
-
-                line_content = hunk.lines[i].content
-                if i in hunk.addlines:
-                    line_content = line_content[1:]  # remove "+"
-
-                if is_comment(line_content):
-                    continue
-
-                line_content = line_content.strip()
-                m = self.pattern.search(line_content)
-                if m:
-                    start_offset = m.start()
-                    prefix = line_content[:start_offset]
-                    prefixes = prefix.split()
-
-                    if 'static' not in prefix:
-                        self.bug_accumulator.append(
-                            BugInstance('SE_NONSTATIC_SERIALVERSIONID', Priorities.NORMAL_PRIORITY, patch.name, hunk.lines[i].lineno[1],
-                                        "serialVersionUID isn't static.")
-                        )
-                    if 'final' not in prefix:
-                        self.bug_accumulator.append(
-                            BugInstance('SE_NONFINAL_SERIALVERSIONID', Priorities.NORMAL_PRIORITY, patch.name, hunk.lines[i].lineno[1],
-                                        "serialVersionUID isn't final.")
-                        )
-                    if 'long' not in prefix:
-                        self.bug_accumulator.append(
-                            BugInstance('SE_NONLONG_SERIALVERSIONID', Priorities.LOW_PRIORITY, patch.name,
-                                        hunk.lines[i].lineno[1],
-                                        "serialVersionUID isn't long.")
-                        )
+        ParentDetector.__init__(self, [
+            DefSerialVersionID(),
+        ])
 
 
+class DefSerialVersionID(SubDetector):
+
+    def __init__(self):
+        self.pattern = re.compile(r'((?:static|final|\s)*)\s+(long|int)\s+serialVersionUID(?!\w\()')
+        SubDetector.__init__(self)
+
+    def match(self, linecontent: str, filename: str, lineno: int):
+        m = self.pattern.search(linecontent)
+        if m:
+            g = m.groups()
+            prefix = None
+            g1 = g[0].strip()
+            if g1:
+                prefix = g1.split()
+
+
+            pattern_name = None
+            message = None
+            priority = Priorities.LOW_PRIORITY
+
+            if prefix:
+                if 'static' not in prefix:
+                    pattern_name = 'SE_NONSTATIC_SERIALVERSIONID'
+                    message = "serialVersionUID isn't static."
+                    priority = Priorities.NORMAL_PRIORITY
+                elif 'final' not in prefix:
+                    pattern_name = 'SE_NONFINAL_SERIALVERSIONID'
+                    message = "serialVersionUID isn't final."
+                    priority = Priorities.NORMAL_PRIORITY
+
+            if not pattern_name and 'int' == g[1]:
+                pattern_name = 'SE_NONLONG_SERIALVERSIONID'
+                message = "serialVersionUID isn't long."
+
+            if pattern_name:
+                self.bug_accumulator.append(
+                    BugInstance(pattern_name, priority, filename, lineno, message))
