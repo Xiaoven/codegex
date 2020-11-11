7ce37363d6b5eb25d7a41869a256bbf44a15491f
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-11-11 19:39:48 +0800

    implement RV_EXCEPTION_NOT_THROWN

---
+++ patterns/detect/method_return_check.py
@@ -0,0 +1,27 @@
+import regex
+
+from patterns.detectors import ParentDetector, SubDetector
+from patterns.bug_instance import BugInstance
+import patterns.priorities as Priorities
+
+
+class MethodReturnCheck(ParentDetector):
+    def __init__(self):
+        ParentDetector.__init__(self, [
+            NotThrowSubDetector()
+        ])
+
+
+class NotThrowSubDetector(SubDetector):
+    def __init__(self):
+        super().__init__()
+        self.pattern = regex.compile(r'^\s*new\s+(\w+)(?:Exception|Error)\s*\(')
+
+    def match(self, linecontent: str, filename: str, lineno: int):
+        m = self.pattern.search(linecontent)
+        if m:
+            self.bug_accumulator.append(
+                BugInstance('RV_EXCEPTION_NOT_THROWN', Priorities.NORMAL_PRIORITY,
+                            filename, lineno,
+                            "Exception created and dropped rather than thrown")
+            )
