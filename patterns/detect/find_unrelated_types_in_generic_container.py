a3bc0f012f31885852a1272893fcca278d04192b
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-11-11 18:04:46 +0800

    implement DMI_VACUOUS_SELF_COLLECTION_CALL

--- patterns/detect/find_unrelated_types_in_generic_container.py
+++ patterns/detect/find_unrelated_types_in_generic_container.py
@@ -8,7 +8,9 @@ import patterns.priorities as Priorities
 class FindUnrelatedTypesInGenericContainer(ParentDetector):
     def __init__(self):
         ParentDetector.__init__(self, [
-            RemoveAllSubDetector()
+            RemoveAllSubDetector(),
+            VacuousSelfCallSubDetector(),
+            NotContainThemselvesSubDetector()
         ])
 
 
@@ -26,6 +28,44 @@ class RemoveAllSubDetector(SubDetector):
             arg = g[1].strip()
             if obj == arg:
                 self.bug_accumulator.append(
-                    BugInstance('DMI_USING_REMOVEALL_TO_CLEAR_COLLECTION', Priorities.HIGH_PRIORITY, filename, lineno,
+                    BugInstance('DMI_USING_REMOVEALL_TO_CLEAR_COLLECTION', Priorities.NORMAL_PRIORITY, filename, lineno,
                                 'removeAll used to clear a collection')
+                )
+
+
+class VacuousSelfCallSubDetector(SubDetector):
+    def __init__(self):
+        self.pattern = re.compile(r'(\w+)\.(?:contains|retain)All\((\w+)\)')
+        SubDetector.__init__(self)
+
+    def match(self, linecontent: str, filename: str, lineno: int):
+        m = self.pattern.search(linecontent)
+        if m:
+            g = m.groups()
+            assert len(g) == 2
+            obj = g[0].strip()
+            arg = g[1].strip()
+            if obj == arg:
+                self.bug_accumulator.append(
+                    BugInstance('DMI_VACUOUS_SELF_COLLECTION_CALL', Priorities.NORMAL_PRIORITY, filename, lineno,
+                                'Vacuous call to collections')
+                )
+
+
+class NotContainThemselvesSubDetector(SubDetector):
+    def __init__(self):
+        self.pattern = re.compile(r'(\w+)\.(?:contains|remove)\((\w+)\)')
+        SubDetector.__init__(self)
+
+    def match(self, linecontent: str, filename: str, lineno: int):
+        m = self.pattern.search(linecontent)
+        if m:
+            g = m.groups()
+            assert len(g) == 2
+            obj = g[0].strip()
+            arg = g[1].strip()
+            if obj == arg:
+                self.bug_accumulator.append(
+                    BugInstance('DMI_COLLECTIONS_SHOULD_NOT_CONTAIN_THEMSELVES', Priorities.HIGH_PRIORITY, filename, lineno,
+                                'Collections should not contain themselves')
                 )
--- tests/test_find_unrelated_types_in_generic_container.py
+++ tests/test_find_unrelated_types_in_generic_container.py
@@ -19,3 +19,58 @@ class TestFindUnrelatedTypesInGenericContainer:
         detector.visit([patch])
         assert len(detector.bug_accumulator) == 1
         assert detector.bug_accumulator[0].line_no == 970
+
+    # From other repositories: https://github.com/erzhen1379/hbase2.1.4/blob/fc65d24aa0043529f3d44ad4b6e50835b0beb056/hbase-common/src/test/java/org/apache/hadoop/hbase/util/TestConcatenatedLists.java#L129
+    def test_DMI_VACUOUS_SELF_COLLECTION_CALL_01(self):
+        patch = Patch()
+        patch.name = "TestConcatenatedLists.java"
+        patch.parse('''@@ -963,6 +967,7 @@   private void verify(ConcatenatedLists<Long> c, int last) {
+            assertEquals((last == -1), c.isEmpty());
+            assertEquals(last + 1, c.size());
+            assertTrue(c.containsAll(c));''')
+        detector = FindUnrelatedTypesInGenericContainer()
+        detector.visit([patch])
+        assert len(detector.bug_accumulator) == 1
+        assert detector.bug_accumulator[0].line_no == 969
+
+    # DIY
+    def test_DMI_VACUOUS_SELF_COLLECTION_CALL_02(self):
+        patch = Patch()
+        patch.name = "OBlock.java"
+        patch.parse('''@@ -963,6 +967,7 @@   private void verify(ConcatenatedLists<Long> c, int last) {
+            assertEquals((last == -1), c.isEmpty());
+            assertEquals(last + 1, c.size());
+            assertTrue(c.retainAll(c));''')
+        detector = FindUnrelatedTypesInGenericContainer()
+        detector.visit([patch])
+        assert len(detector.bug_accumulator) == 1
+        assert detector.bug_accumulator[0].line_no == 969
+
+    # https://github.com/josephearl/findbugs/blob/fd7ec8b5cc0b1b143589674cdcdb901fa5dc0dda/findbugsTestCases/src/java/gcUnrelatedTypes/Ideas_2011_06_30.java#L13
+    def test_DMI_COLLECTIONS_SHOULD_NOT_CONTAIN_THEMSELVES_01(self):
+        patch = Patch()
+        patch.name = "Ideas_2011_06_30.java"
+        patch.parse('''@@ -963,6 +967,7 @@
+                   @ExpectWarning("DMI_COLLECTIONS_SHOULD_NOT_CONTAIN_THEMSELVES")
+                    public static void testTP(Collection<Integer> c) {
+                        assertTrue(c.contains(c));
+                    }''')
+        detector = FindUnrelatedTypesInGenericContainer()
+        detector.visit([patch])
+        assert len(detector.bug_accumulator) == 1
+        assert detector.bug_accumulator[0].line_no == 969
+
+    # DIY
+    def test_DMI_COLLECTIONS_SHOULD_NOT_CONTAIN_THEMSELVES_02(self):
+        patch = Patch()
+        patch.name = "Ideas_2011_06_30.java"
+        patch.parse('''@@ -963,6 +967,7 @@
+                        @ExpectWarning("DMI_COLLECTIONS_SHOULD_NOT_CONTAIN_THEMSELVES")
+                        public static void testTP(Collection<Integer> c) {
+                            return c.remove(c);
+                        }
+            ''')
+        detector = FindUnrelatedTypesInGenericContainer()
+        detector.visit([patch])
+        assert len(detector.bug_accumulator) == 1
+        assert detector.bug_accumulator[0].line_no == 969
