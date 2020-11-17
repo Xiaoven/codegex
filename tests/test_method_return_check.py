2a2d55b033e20057f6db196890603e69a038620e
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-11-17 17:01:33 +0800

    Implement RV_EXCEPTION_NOT_THROWN (#45)
    * implement RV_EXCEPTION_NOT_THROWN
    
    * Revert "implement RV_EXCEPTION_NOT_THROWN"
    
    This reverts commit 7ce37363d6b5eb25d7a41869a256bbf44a15491f.
    
    * implement RV_EXCEPTION_NOT_THROWN
    
    Adapt to the new parser to pass tests

---
+++ tests/test_method_return_check.py
@@ -0,0 +1,39 @@
+import pytest
+
+from patterns.detect.method_return_check import MethodReturnCheck
+from rparser import Patch, parse
+
+params = [
+    # https://github.com/spotbugs/spotbugs/blob/3883a7b750fb339577be073bc45e36b6f268777b/spotbugsTestCases/src/java/bugIdeas/Ideas_2011_11_02.java
+    ( 'RV_EXCEPTION_NOT_THROWN', 'Ideas_2011_11_02.java',
+     '''@@ -11,3 +11,3 @@ public class Ideas_2011_11_02 {
+            public void setCheckedElements(Object[] elements) {
+                 new UnsupportedOperationException();
+        }''', 1, 12),
+    # https://github.com/bndtools/bnd/commit/960664b12a8f8886779617a283883cdc901cef5e
+    ( 'RV_EXCEPTION_NOT_THROWN', 'Clazz.java',
+     '''@@ -1114,6 +1114,7 @@ void doSignature(DataInputStream in, ElementType member, int access_flags) throw
+				classSignature = signature;
+
+		} catch (Exception e) {
++	        new RuntimeException("Signature failed for" + signature, e);
+		}
+	}
+
+        ''', 1, 1117),
+]
+
+
+@pytest.mark.parametrize('pattern_type,file_name,patch_str,expected_length,line_no', params)
+def test(pattern_type: str, file_name: str, patch_str: str, expected_length: int, line_no: int):
+    patch = parse(patch_str)
+    patch.name = file_name
+
+    detector = MethodReturnCheck()
+    detector.visit([patch])
+    if expected_length > 0:
+        assert len(detector.bug_accumulator) == expected_length
+        assert detector.bug_accumulator[0].line_no == line_no
+        assert detector.bug_accumulator[0].type == pattern_type
+    else:
+        assert len(detector.bug_accumulator) == 0
