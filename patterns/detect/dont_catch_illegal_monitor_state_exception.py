e29536c2291f3af34aaf04bb8d7915d4dd264a0a
xiaoven <xiaoven.cheung@gmail.com>
2020-12-09 11:30:02 +0800

    Rewrite DontCatchIllegalMonitorStateException

--- patterns/detect/dont_catch_illegal_monitor_state_exception.py
+++ patterns/detect/dont_catch_illegal_monitor_state_exception.py
@@ -1,12 +1,11 @@
 import re
 
-from patterns.detectors import Detector
-import patterns.utils as utils
+from patterns.detectors import SubDetector
 from patterns.bug_instance import BugInstance
 import patterns.priorities as Priorities
 
 
-class DontCatchIllegalMonitorStateException(Detector):
+class DontCatchIllegalMonitorStateException(SubDetector):
     '''
     匹配 catch (xxException e1, ..., xxException e2), 并分离出 Exception 类型
     检查到 catch 关键词, 开启多行模式
@@ -15,26 +14,26 @@ class DontCatchIllegalMonitorStateException(Detector):
 
     def __init__(self):
         # 匹配形如 "catch (IOException e)"
-        self.regexp = re.compile(r'catch\s*\(\s*([a-zA-Z0-9\.]+\s+[a-zA-Z0-9\.]+.*?)\)')
-        self.regexp2 = re.compile(r'([a-zA-Z0-9\.]+)\s+[a-zA-Z0-9\.]+')  # 提取类名
+        self.p_catch = re.compile(r'catch\s*\(([^()]+)\)')
+        SubDetector.__init__(self)
 
-    def _visit_patch(self, patch):
-        for hunk in patch:
-            idx_add_lines = hunk.addlines
-            for i in idx_add_lines:
-                content = hunk.lines[i].content[1:]  # 移除开头的 '+'
+    def match(self, linecontent: str, filename: str, lineno: int, get_exact_lineno=None):
+        match = self.p_catch.search(linecontent)
+        if lineno == 4:
+            print(lineno)
+        if match:
+            params = match.groups()[0]
+            defs = params.split(',')
+            for d in defs:
+                exception_class = d.split()[0]
 
-                if utils.is_comment(content):
-                    continue
+                if get_exact_lineno:
+                    lineno = get_exact_lineno(exception_class)[0]
 
-                match = self.regexp.search(content)
-                if match:
-                    exp_stat = match.groups()[0]
-                    exp_types = self.regexp2.findall(exp_stat)
-                    for exp in exp_types:
-                        if exp.endswith('IllegalMonitorStateException'):
-                            self.bug_accumulator.append(
-                                BugInstance('IMSE_DONT_CATCH_IMSE', Priorities.HIGH_PRIORITY,
-                                            patch.name, hunk.lines[i].lineno[1],
-                                            'Dubious catching of IllegalMonitorStateException')
-                            )
+                if exception_class.endswith('IllegalMonitorStateException'):
+                    self.bug_accumulator.append(
+                        BugInstance('IMSE_DONT_CATCH_IMSE', Priorities.HIGH_PRIORITY,
+                                    filename, lineno,
+                                    'Dubious catching of IllegalMonitorStateException')
+                    )
+                    break
--- tests/dont_catch_illegal_monitor_state_exception.py
+++ tests/dont_catch_illegal_monitor_state_exception.py
@@ -1,12 +1,32 @@
 import pytest
 
+from patterns.detectors import ParentDetector
 from rparser import parse
 from patterns.detect.dont_catch_illegal_monitor_state_exception import DontCatchIllegalMonitorStateException
 
-params = [(True, DontCatchIllegalMonitorStateException(), 'IMSE_DONT_CATCH_IMSE', 'Fake.java',
-           "@@ -146,11 +147,7 @@ public void serviceAdded(ServiceEvent se) {\n             log.debug(\"Service added: {}\", se.getInfo().toString());\n             // notify the client when a service is added.\n             synchronized (client) {\n+                try {\n+                    client.notifyAll();\n+                } catch (java.lang.IllegalMonitorStateException imse) {\n+                    log.error(\"Error notifying waiting listeners: {}\", imse.getCause());\n+                }\n-                client.notifyAll();\n             }\n         }\n",
+params = [
+    (True, DontCatchIllegalMonitorStateException(), 'IMSE_DONT_CATCH_IMSE', 'Fake.java',
+           '''@@ -146,11 +147,7 @@ public void serviceAdded(ServiceEvent se) {
+             log.debug("Service added: {}", se.getInfo().toString());
+             // notify the client when a service is added.
+             synchronized (client) {
++                try {
++                    client.notifyAll();
++                } catch (java.lang.IllegalMonitorStateException imse) {
++                    log.error("Error notifying waiting listeners: {}", imse.getCause());
++                }
+-                client.notifyAll();
+             }
+         }''',
            1, 152),
-          ]
+    (False, DontCatchIllegalMonitorStateException(), 'IMSE_DONT_CATCH_IMSE', 'Fake.java',
+        '''
+        } catch (IOException io, 
+                    java.lang.IllegalMonitorStateException imse) {
+                        log.error("Error notifying waiting listeners: {}", imse.getCause());
+                }
+        ''', 1, 3),
+]
 
 
 @pytest.mark.parametrize('is_patch,detector,pattern_type,file_name,patch_str,expected_length,line_no', params)
@@ -14,10 +34,11 @@ def test(is_patch: bool, detector, pattern_type: str, file_name: str, patch_str:
          line_no: int):
     patch = parse(patch_str, is_patch)
     patch.name = file_name
-    detector.visit([patch])
+    engine = ParentDetector([detector])
+    engine.visit([patch])
     if expected_length > 0:
-        assert len(detector.bug_accumulator) == expected_length
-        assert detector.bug_accumulator[0].line_no == line_no
-        assert detector.bug_accumulator[0].type == pattern_type
+        assert len(engine.bug_accumulator) == expected_length
+        assert engine.bug_accumulator[0].line_no == line_no
+        assert engine.bug_accumulator[0].type == pattern_type
     else:
-        assert len(detector.bug_accumulator) == 0
+        assert len(engine.bug_accumulator) == 0
