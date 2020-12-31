12045e62e470a4f6dbe0c078b8f2a0d91b3124dc
Xiaoven <sylvia.lovely.me@gmail.com>
2021-01-01 00:46:30 +0800

    [Part 1] Refactor gen_detectors.py to add cache

--- gen_detectors.py
+++ gen_detectors.py
@@ -1,22 +1,38 @@
+from patterns.detect.inheritance_unsafe_get_resource import GetResourceDetector
+from patterns.detect.incompat_mask import IncompatMaskDetector
+from patterns.detect.serializable_idiom import DefSerialVersionID, DefReadResolveMethod
 from patterns.detect.dont_catch_illegal_monitor_state_exception import DontCatchIllegalMonitorStateException
-from patterns.detect.dumb_methods import StringCtorDetector, RandomD2IDetector, RandomOnceDetector, \
-    FinalizerOnExitDetector
+from patterns.detect.find_rough_constants import FindRoughConstantsDetector
+from patterns.detect.naming import SimpleNameDetector1, SimpleNameDetector2
+from patterns.detect.method_return_check import NotThrowDetector
+from patterns.detect.format_string_checker import NewLineDetector
 from patterns.detect.find_finalize_invocations import ExplicitInvDetector, PublicAccessDetector
 from patterns.detect.find_ref_comparison import EqualityDetector, CalToNullDetector
-from patterns.detect.find_rough_constants import FindRoughConstantsDetector
-from patterns.detect.find_unrelated_types_in_generic_container import SuspiciousCollectionMethodDetector
-from patterns.detect.format_string_checker import NewLineDetector
-from patterns.detect.incompat_mask import IncompatMaskDetector
-from patterns.detect.infinite_recursive_loop import CollectionAddItselfDetector
-from patterns.detect.inheritance_unsafe_get_resource import GetResourceDetector
-from patterns.detect.method_return_check import NotThrowDetector
-from patterns.detect.naming import SimpleNameDetector2, SimpleNameDetector1
-from patterns.detect.serializable_idiom import DefSerialVersionID, DefReadResolveMethod
 from patterns.detect.static_calendar_detector import StaticDateFormatDetector
+from patterns.detect.infinite_recursive_loop import CollectionAddItselfDetector
+from patterns.detect.find_unrelated_types_in_generic_container import SuspiciousCollectionMethodDetector
+from patterns.detect.dumb_methods import FinalizerOnExitDetector, RandomOnceDetector, RandomD2IDetector, StringCtorDetector
 
-DETECTORS = [IncompatMaskDetector(), GetResourceDetector(), StaticDateFormatDetector(), NotThrowDetector(),
-             CollectionAddItselfDetector(), FindRoughConstantsDetector(), EqualityDetector(), CalToNullDetector(),
-             SimpleNameDetector1(), SimpleNameDetector2(), DontCatchIllegalMonitorStateException(),
-             ExplicitInvDetector(), PublicAccessDetector(), DefSerialVersionID(), DefReadResolveMethod(),
-             SuspiciousCollectionMethodDetector(), FinalizerOnExitDetector(), RandomOnceDetector(), RandomD2IDetector(),
-             StringCtorDetector(), NewLineDetector()]
+DETECTOR_DICT = {
+    "GetResourceDetector": GetResourceDetector,
+    "IncompatMaskDetector": IncompatMaskDetector,
+    "DefSerialVersionID": DefSerialVersionID,
+    "DefReadResolveMethod": DefReadResolveMethod,
+    "DontCatchIllegalMonitorStateException": DontCatchIllegalMonitorStateException,
+    "FindRoughConstantsDetector": FindRoughConstantsDetector,
+    "SimpleNameDetector1": SimpleNameDetector1,
+    "SimpleNameDetector2": SimpleNameDetector2,
+    "NotThrowDetector": NotThrowDetector,
+    "NewLineDetector": NewLineDetector,
+    "ExplicitInvDetector": ExplicitInvDetector,
+    "PublicAccessDetector": PublicAccessDetector,
+    "EqualityDetector": EqualityDetector,
+    "CalToNullDetector": CalToNullDetector,
+    "StaticDateFormatDetector": StaticDateFormatDetector,
+    "CollectionAddItselfDetector": CollectionAddItselfDetector,
+    "SuspiciousCollectionMethodDetector": SuspiciousCollectionMethodDetector,
+    "FinalizerOnExitDetector": FinalizerOnExitDetector,
+    "RandomOnceDetector": RandomOnceDetector,
+    "RandomD2IDetector": RandomD2IDetector,
+    "StringCtorDetector": StringCtorDetector
+}
