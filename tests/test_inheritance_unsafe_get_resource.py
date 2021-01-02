dce134f9892837d65c24589f9e09284551fe2406
Xiaoven <sylvia.lovely.me@gmail.com>
2021-01-03 01:11:16 +0800

    skip test_online_search() if you don't modify this detector

--- tests/test_inheritance_unsafe_get_resource.py
+++ tests/test_inheritance_unsafe_get_resource.py
@@ -55,7 +55,7 @@ class ShareCacheEngine(DefaultEngine):
             self._visit_patch(patch)
 
 
-# @pytest.mark.skip(reason="Time consuming due to involving network requests")
+@pytest.mark.skip(reason="Time consuming due to involving network requests")
 def test_online_search():
     patch_1 = parse('''File expectedFile = new File(getClass().getResource(name).getFile());''', False)
     patch_1.name = 'animated-gif-lib-for-java/src/main/java/com/madgag/gif/fmsware/NanoHTTPD.java'
