dfddb51dbe5b05b7844736e207ad983751708a59
zhouyinn <yinnzho@gmail.com>
2021-01-13 21:44:26 +0800

    modify as suggestion

--- patterns/detect/find_float_equality.py
+++ patterns/detect/find_float_equality.py
@@ -6,23 +6,31 @@ from patterns.models import priorities
 
 class FloatEqualityDetector(Detector):
     def __init__(self):
-        self.pattern = regex.compile(
-            r'((?:(?P<aux1>\((?:[^()]++|(?&aux1))*\))|[\w."])++)\s*[<>!=]+\s*((?:(?&aux1)|[\w."])+)')
-        self.float_NaN = ('Float.NaN', 'Double.NaN')
+        self.pattern_op = regex.compile(
+            r'(\b\w[\w.]*(?P<aux1>\((?:[^()]++|(?&aux1))*\))*)\s*[<>!=]+\s*(\b\w[\w.]*(?&aux1)*)')
+        self.pattern_nan = regex.compile(r'\b\w+\.NaN\b')
         Detector.__init__(self)
 
     def match(self, linecontent: str, filename: str, lineno: int, **kwargs):
-        if any(op in linecontent for op in ('>', '<', '>=', '<=', '==', '!=')):
-            its = self.pattern.finditer(linecontent.strip())
+        strip_line = linecontent.strip()
+        if self.pattern_nan.search(strip_line) \
+                and any(op in linecontent for op in ('>', '<', '>=', '<=', '==', '!=')):
+            its = self.pattern_op.finditer(strip_line)
             for m in its:
                 op_1 = m.groups()[0]  # m.groups()[1] is the result of named pattern
                 op_2 = m.groups()[2]
-                if op_1 in self.float_NaN and op_2 in self.float_NaN:
+                if self.is_float(op_1) and self.is_float(op_2):
                     return
-                if op_1 in self.float_NaN or op_2 in self.float_NaN:
+                if self.is_float(op_1) or self.is_float(op_2):
                     self.bug_accumulator.append(
                     BugInstance('FE_TEST_IF_EQUAL_TO_NOT_A_NUMBER', priorities.HIGH_PRIORITY,
                                 filename, lineno,
                                 "Doomed test for equality to NaN")
                     )
                 break
+
+    def is_float(self, oprand:str):
+        if "Double" in oprand or "Float" in oprand:
+            return True
+        else:
+            return False
