70a6f30057fe7d11f6032008d5ccb43004675ad8
Xiaoven <sylvia.lovely.me@gmail.com>
2021-01-22 14:36:12 +0800

    Update find_missing_pr.py

--- find_missing_pr.py
+++ find_missing_pr.py
@@ -11,17 +11,17 @@ if __name__ == '__main__':
     for p in paths:
         with open(p, 'r') as f:
             for line in f:
-                strip_line = line.strip()
-                if strip_line:
+                link = line.strip().split(',')[0]
+                if link:
                     total_link_cnt += 1
 
-                    if strip_line in link_dic:
-                        link_dic[strip_line] += 1
+                    if link in link_dic:
+                        link_dic[link] += 1
                         # print('[Duplicate]: ', line)
                     else:
-                        link_dic[strip_line] = 0
+                        link_dic[link] = 0
 
-                    file_path = strip_line.replace('https://api.github.com/repos/',
+                    file_path = link.replace('https://api.github.com/repos/',
                                                    'PullRequests/java/files/') + '.json'
 
                     if not path.exists(file_path):
