58db0e793d59c584eff007874fbc7cf96a2577b1
Xiaoven <55338943+Xiaoven@users.noreply.github.com>
2021-03-18 21:03:31 +0800

    Update exp_comparison.py (#164)
    Get files to analyze by spotbugs' execution log

--- exp_comparison.py
+++ exp_comparison.py
@@ -2,20 +2,27 @@ import os
 from os import path
 from subprocess import check_output
 import re
+import json
 
 from patterns.models.context import Context
 from patterns.models.engine import DefaultEngine
 from rparser import parse
 from timer import Timer
 
-# BASE_PATH = '/home/xiaowen/Project/rbugs_experiments/github-repos'
 BASE_PATH = '/Users/audrey/Documents/GitHub/rbugs/comparison'
-LOG_PATH = 'comparison_exp'
+REPORT_PATH = BASE_PATH + '/report'
+REPO_PATH = BASE_PATH + '/repos'
+EXEC_LOG_PATH = BASE_PATH + '/execute_log'
+EXECUTE_PATH = BASE_PATH + '/exec_path'
+os.makedirs(EXECUTE_PATH, exist_ok=True)
+os.makedirs(REPORT_PATH, exist_ok=True)
 
 TASK_MAIN = 'task_main'
 TASK_TEST = 'task_test'
 
 JAVA_FILE_PATTERN = re.compile(r'^(.+?)/src/(main|test)/java/')
+
+ANALYZE_FILE = re.compile(r'\[java\] \w+\s+\d+: Analyzing classes \(')
 
 
 def empty_file(file_path: str):
@@ -32,111 +39,85 @@ def write_file(file_path: str, content: str):
         f.write(content)
 
 
-def exec_task(file_dict: dict):
-    bug_instance_dict = dict()
+def exec_task(file_list):
+    bug_instances = list()
     context = Context()
     engine = DefaultEngine(context)
 
-    for subproject, file_list in file_dict.items():
-        # generate patch set
-        patchset = []
-        for file_path in file_list:
-            with open(file_path, 'r') as f:
-                patch = parse(f.read(), is_patch=False, name=file_path)
-                patchset.append(patch)
+    # generate patch set
+    patchset = []
+    for file_path in file_list:
+        with open(file_path, 'r') as f:
+            patch = parse(f.read(), is_patch=False, name=file_path)
+            patchset.append(patch)
 
-        # run detectors
-        if not patchset:
-            continue
-
+    # run detectors
+    if patchset:
         engine.visit(*patchset)
+        bug_instances = engine.filter_bugs(level='low')
 
-        if engine.bug_accumulator:
-            bug_instance_dict[subproject] = engine.bug_accumulator
-    return bug_instance_dict
+    return bug_instances
 
 
-def detect_project(project_name: str, tasks=(TASK_MAIN, TASK_TEST)):
-    # get java files
-    source_paths = dict()
-    test_paths = dict()
-    other_paths = list()
-    project_path = path.join(BASE_PATH, project_name)
-    output = check_output(['find', project_path, '-name', '*.java']).decode('utf-8')
-    if not output.strip():
-        return
+def gen_analyze_paths():
+    ls_result = os.listdir(EXEC_LOG_PATH)
+    project_names = [name.rstrip('.log') for name in ls_result if name.endswith('.log')]
 
-    cnt_source, cnt_test = 0, 0
+    for name in project_names:
+        files_to_analyze = set()
+        with open(path.join(EXEC_LOG_PATH, name + '.log'), 'r') as f:
+            for line in f:
+                line = line.strip()
+                if ANALYZE_FILE.match(line):
+                    tmp = line.split()
+                    if tmp:
+                        output = check_output(['find', path.join(REPO_PATH, name), '-path', f'*{tmp[-1]}.java']).decode('utf-8')
+                        if output.strip():
+                            for p in output.splitlines():
+                                if '/src/main/java/' in p or '/src/test/java/' in p:
+                                    files_to_analyze.add(p.strip())
+        if files_to_analyze:
+            with open(path.join(EXECUTE_PATH, name + '.json'), 'w') as out:
+                json.dump(list(files_to_analyze), out)
 
-    for file_path in output.splitlines():
-        m = JAVA_FILE_PATTERN.match(file_path)
-        if m:
-            g = m.groups()
 
-            if g[1] == 'main' in file_path:
-                tmp_paths = source_paths.get(g[0], list())
-                tmp_paths.append(file_path)
-                source_paths[g[0]] = tmp_paths
-                cnt_source += 1
-            elif g[1] == 'test' in file_path:
-                tmp_paths = source_paths.get(g[0], list())
-                tmp_paths.append(file_path)
-                test_paths[g[0]] = tmp_paths
-                cnt_test += 1
-            else:
-                other_paths.append(file_path)
+def detect_project(project_name: str):
+    # load files to analyze
+    with open(path.join(EXECUTE_PATH, project_name + '.json'), 'r') as f:
+        path_list = json.load(f)
 
-            # if cnt_source + cnt_test >= 100:
-            #     break
-
-    log_dir = path.join(LOG_PATH, project_name)
-    os.makedirs(log_dir, exist_ok=True)
-    logfile = path.join(log_dir, 'detect.log')
-    empty_file(logfile)
-
-    append_file(logfile, f'source file num: {cnt_source}\ntest file num: {cnt_test}\n')
-
-    if other_paths:
-        append_file(logfile, '------------------------- Unknown Java Files -------------------------\n'
-                    + '\n'.join(other_paths) + '\n')
-
-    unknown_tasks = list()
-    for task in tasks:
-        bug_ins_dict = dict()
-        log_file_name = ''
-        if task == TASK_MAIN:
-            append_file(logfile, '------------------------- Execute Task Main -------------------------\n')
-            bug_ins_dict = exec_task(source_paths)
-            log_file_name = 'main.csv'
-        elif task == TASK_TEST:
-            append_file(logfile, '------------------------- Execute Task Test -------------------------\n')
-            bug_ins_dict = exec_task(test_paths)
-            log_file_name = 'test.csv'
-        else:
-            unknown_tasks.append(task)
+    if path_list:
+        # analyze files
+        bug_instances = exec_task(path_list)
 
         # write reports
-        for subproject, bug_instances in bug_ins_dict.items():
-            if bug_instances:
-                report_path = subproject.replace(BASE_PATH, log_dir)
-                os.makedirs(report_path, exist_ok=True)
-                report_path = path.join(report_path, log_file_name)
-                content = str()
-                for ins in bug_instances:
-                    content += f'{ins.type},{ins.file_name},{ins.line_no}\n'
-                write_file(report_path, content)
-                append_file(logfile, f'Found {len(bug_instances)} violations: f{report_path}\n')
+        if bug_instances:
+            repo_report_path = path.join(REPORT_PATH, project_name)
+            os.makedirs(repo_report_path, exist_ok=True)
+            # write csv
+            content = str()
+            for ins in bug_instances:
+                content += f'{ins.type},{ins.file_name},{ins.line_no}\n'
 
-    if unknown_tasks:
-        append_file(logfile, '------------------------- Unknown Tasks -------------------------\n'
-                    + '\n'.join(unknown_tasks) + '\n')
+            write_file(f'{repo_report_path}/{project_name}.csv', content)
+            # write bug instance json
+            with open(f'{repo_report_path}/bug_instances.json', 'w') as out:
+                bugs_json = dict()
+                bugs_json['repo'] = project_name
+                bugs_json['total'] = len(bug_instances)
+                bugs_json['items'] = [bug.__dict__ for bug in bug_instances]
+                json.dump(bugs_json, out)
 
 
 if __name__ == '__main__':
+    # Step 1: get files to analyze by spotbugs' execution log
+    gen_analyze_paths()
+
+    # Step 2: run rbugs against the files in Step 1
     total_timer = Timer(name='Total', logger=None)
     total_timer.start()
 
-    project_name_list = os.listdir(BASE_PATH)
+    project_name_list = os.listdir(REPO_PATH)
     for project_name in project_name_list:
         project_timer = Timer(name=project_name, logger=None)
         project_timer.start()
@@ -147,7 +128,8 @@ if __name__ == '__main__':
 
     total_timer.stop()
 
-    time_log = path.join(LOG_PATH, 'time.log')
+    # Step 3: write time dict to file
+    time_log = path.join(REPORT_PATH, 'time.log')
     with open(time_log, 'w') as f:
         f.write('\n'.rjust(41, '=') + f'Total Time: {Timer.timers["Total"]}\n' + '\n'.rjust(41, '=') + '\n')
         for project_name in project_name_list:
