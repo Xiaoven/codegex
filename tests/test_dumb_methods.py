0602a2d3a68f0e0443afc9234c48415ae92cd9f5
xiaoven <xiaoven.cheung@gmail.com>
2021-12-14 17:59:44 +0800

    add test cases

--- tests/test_dumb_methods.py
+++ tests/test_dumb_methods.py
@@ -54,7 +54,7 @@ params = [
         pool.setMaxWaitMillis(-1L);
         pool.setBlockWhenExhausted(true);
 +       pool.setMinEvictableIdleTimeMillis(threadLifetimeMs());
-+       pool.setTimeBetweenEvictionRunsMillis(threadLifetimeMs() + 
++       pool.setTimeBetweenEvictionRunsMillis(threadLifetimeMs() +
 +                                               new Random(threadLifetimeMs()).nextLong());
 +    return pool;''', 1, 405),
     # From other repository: https://github.com/adaptris/interlok/commit/8dd32e9b89a4b17662faa7ca986756f3cc348cc7#diff-766b5e25592ad321e107f1f856d8a08bL102
@@ -245,8 +245,12 @@ params = [
      '''for (; i < n; i += (1 + (int) (jump * r.nextDouble ( ) ))) ''', 1, 1),
     # From Github: https://github.com/justinjamesob/device_sprd/commit/b5d72a8df2606459a8ad5349613749bd572f175a
     (False, 'DM_NEXTINT_VIA_NEXTDOUBLE', 'TestNextIntViaNextDoubleDetector_02.java',
-     '''int index = (int) (Math.random() * 3);''', 1, 1)
-
+     '''int index = (int) (Math.random() * 3);''', 1, 1),
+    # DIY
+    (False, 'DM_NEXTINT_VIA_NEXTDOUBLE', 'TestNextIntViaNextDoubleDetector_03.java',
+     '''return (int)(myrandom() * 3);''', 0, 0),
+    (False, 'DM_NEXTINT_VIA_NEXTDOUBLE', 'TestNextIntViaNextDoubleDetector_04.java',
+     '''return (int) (1.2 * r.nextDouble());''', 1, 1),
 ]
 
 
